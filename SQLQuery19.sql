d1dbefaddd2b9c06d7ccdfc3c8830fc4e0ff08a5
ganeshraju1 <139103313+ganeshraju1@users.noreply.github.com>
2023-08-07 22:23:12 +0530

    Update SQLQuery19.sql

--- SQLQuery19.sql
+++ SQLQuery19.sql
@@ -1,3 +1,14 @@
-/* Write a query using the Sales.SalesOrderDetail table to display a value (Under 10 or 10–19 or 20–29 
-or 30–39 or 40 and over) based on the OrderQty value by using the CASE expression. Include the 
-SalesOrderID and OrderQty columns in the results.*/use AdventureWorks2022select SalesOrderID ,OrderQty,casewhen  OrderQty<10 then 'under 10'when  OrderQty>=10 and OrderQty<19 then '10–19'when  OrderQty>=20 and OrderQty<29  then '20–29'when  OrderQty>=30 and OrderQty<39 then '30–39'else 'above 40'end as resultfrom Sales.SalesOrderDetail
+/* Write a query using the Sales.SalesOrderDetail table to display a value (Under 10 or 10â€“19 or 20â€“29 
+or 30â€“39 or 40 and over) based on the OrderQty value by using the CASE expression. Include the 
+SalesOrderID and OrderQty columns in the results.*/
+use AdventureWorks2022
+select SalesOrderID ,OrderQty,
+case
+when  OrderQty<10 then 'under 10'
+when  OrderQty>=10 and OrderQty<19 then '10â€“19'
+when  OrderQty>=20 and OrderQty<29  then '20â€“29'
+when  OrderQty>=30 and OrderQty<39 then '30â€“39'
+else 'above 40'
+end as result
+from Sales.SalesOrderDetail
+
