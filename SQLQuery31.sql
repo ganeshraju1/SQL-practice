91427daed21071550392fa45f64b6474142081c0
ganeshraju1 <139103313+ganeshraju1@users.noreply.github.com>
2023-08-19 09:05:56 +0530

    Add files via upload

---
+++ SQLQuery31.sql
@@ -0,0 +1,2 @@
+/*2. Using a subquery that includes the Sales.SalesOrderDetail table, display the product names and 
+product ID numbers from the Production.Product table to display the products that have not been ordered*/use AdventureWorks2022select name,ProductID from Production.Productwhere productid  not in (select productid from Sales.SalesOrderDetail)
