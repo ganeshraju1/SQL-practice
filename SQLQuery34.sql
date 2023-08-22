1a23471539c80f64233cf7b66b9cdaf95d5513b5
ganeshraju1 <139103313+ganeshraju1@users.noreply.github.com>
2023-08-22 11:04:59 +0530

    Update SQLQuery34.sql

--- SQLQuery34.sql
+++ SQLQuery34.sql
@@ -4,8 +4,8 @@ Display the SalesOrderID, OrderDate, and ProductID columns in the results. The
 Sales.SalesOrderDetail table should be inside the derived table query.*/
 use AdventureWorks2022
 select soh.SalesOrderID,soh.OrderDate,sod.ProductID from Sales.SalesOrderHeader as soh
-
-inner join (select * from Sales.SalesOrderDetail) as sod on 
+inner join 
+(select * from Sales.SalesOrderDetail) as sod on 
 soh.SalesOrderID=sod.SalesOrderID
 
 
