5beff94200deeb930b0ef84a09c684a968ada7f0
ganeshraju1 <139103313+ganeshraju1@users.noreply.github.com>
2023-08-23 09:37:24 +0530

    Add files via upload

---
+++ SQLQuery35.sql
@@ -0,0 +1,9 @@
+/* using cte join the Sales.SalesOrderHeader table to the Sales.SalesOrderDetail table. 
+Display the SalesOrderID, OrderDate, and ProductID columns in the results. The 
+Sales.SalesOrderDetail table should be inside common table expression*/
+use AdventureWorks2022;
+with sod AS( select  SalesOrderID, ProductID from Sales.SalesOrderDetail)
+select soh.SalesOrderID,soh.OrderDate,sod.ProductID from Sales.SalesOrderHeader as soh
+inner join sod on
+soh.SalesOrderID=sod.SalesOrderID
+
