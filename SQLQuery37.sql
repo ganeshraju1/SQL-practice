751a49cdeaa155615e7b32a3f24a7de88f26ca3c
ganeshraju1 <139103313+ganeshraju1@users.noreply.github.com>
2023-08-26 23:25:15 +0530

    Add files via upload

---
+++ SQLQuery37.sql
@@ -0,0 +1,15 @@
+/*Write a query that assigns row numbers to the Production.Product table. Start the numbers over for 
+each ProductSubCategoryID and make sure that the row numbers are in order of ProductID. Display 
+only rows where the ProductSubCategoryID is not null.*/
+use AdventureWorks2022
+SELECT
+    ProductID,
+    Name,
+    ProductNumber,
+    ProductSubCategoryID,
+    ROW_NUMBER() OVER(PARTITION BY ProductSubCategoryID ORDER BY ProductID) AS RowNumber
+FROM
+    Production.Product
+WHERE
+    ProductSubCategoryID IS NOT NULL;
+
