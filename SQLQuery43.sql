ae3d8f9fa054f4613e4f4174154b0661d6346435
ganeshraju1 <139103313+ganeshraju1@users.noreply.github.com>
2023-09-01 11:22:30 +0530

    Add files via upload

---
+++ SQLQuery43.sql
@@ -0,0 +1,1 @@
+/*Change the query in Question 5 so that only the products with the color blue or red are included.*/use AdventureWorks2022select name,colorfrom Production.Productgroup by name,colorhaving color='blue' or color='red'
