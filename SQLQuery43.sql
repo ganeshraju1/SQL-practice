11f40e251ed17b9c3fe012f991071773b42cb53c
ganeshraju1 <139103313+ganeshraju1@users.noreply.github.com>
2023-09-01 11:23:07 +0530

    Update SQLQuery43.sql

--- SQLQuery43.sql
+++ SQLQuery43.sql
@@ -1,1 +1,6 @@
-/*Change the query in Question 5 so that only the products with the color blue or red are included.*/use AdventureWorks2022select name,colorfrom Production.Productgroup by name,colorhaving color='blue' or color='red'
+/*Change the query in Question 5 so that only the products with the color blue or red are included*/
+use AdventureWorks2022
+select name,color
+from Production.Product
+group by name,color
+having color='blue' or color='red'
