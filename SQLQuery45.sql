09464fd122f6bdc5950bc8ff25216b2766307332
ganeshraju1 <139103313+ganeshraju1@users.noreply.github.com>
2023-09-04 15:41:02 +0530

    Update SQLQuery45.sql

--- SQLQuery45.sql
+++ SQLQuery45.sql
@@ -1,15 +1,16 @@
 /*Write a query joining the Person.Person, Sales.Customer, and Sales.SalesOrderHeader tables to 
-return a list of the customer names along with a count of the orders placed*/use AdventureWorks2022SELECT
-    CONCAT(p.FirstName, ' ', p.LastName) AS CustomerName,
-    COUNT(soh.SalesOrderID) AS OrderCount
-FROM
-    Person.Person AS p
+return a list of the customer names along with a count of the orders placed*/
+use AdventureWorks2022;
+SELECT
+CONCAT(p.FirstName, ' ', p.LastName) AS CustomerName,
+COUNT(soh.SalesOrderID) AS OrderCount
+FROM Person.Person AS p
 INNER JOIN
-    Sales.Customer AS c ON p.BusinessEntityID = c.PersonID
+Sales.Customer AS c ON p.BusinessEntityID = c.PersonID
 LEFT JOIN
-    Sales.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID
+Sales.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID
 GROUP BY
-    CONCAT(p.FirstName, ' ', p.LastName)
+CONCAT(p.FirstName, ' ', p.LastName)
 ORDER BY
-    OrderCount DESC;
-
+OrderCount DESC;
+
